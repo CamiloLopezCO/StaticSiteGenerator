ef17a91db2d73400a0ed165c85b6514cae6adec4
CamiloLopezCO <camilolvh93@gmail.com>
2025-04-26 13:15:07 -0400

    tracing bug

--- src/test_markdown_to_html_node.py
+++ src/test_markdown_to_html_node.py
@@ -18,8 +18,12 @@ This is another paragraph with _italic_ text and `code` here
         )
 
     def test_codeblock(self):
-        md = """```This is text that _should_ remain
-the **same** even with inline stuff```"""
+        md = """
+    ```
+    This is text that _should_ remain
+    the **same** even with inline stuff
+    ```
+    """
         node = markdown_to_html_node(md)
         html = node.to_html()
         self.assertEqual(
