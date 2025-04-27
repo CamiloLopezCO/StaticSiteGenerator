bd0393fb4956f7b352041c2cf1100815877e196f
CamiloLopezCO <camilolvh93@gmail.com>
2025-04-27 08:43:44 -0400

    Modified main.py with a recursive function to copy all the contents from a srouce dir to a destination dir

--- src/main.py
+++ src/main.py
@@ -1,9 +1,27 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+def clear_and_copy_static(src_dir, dest_dir):
+    if os.path.exists(dest_dir):
+        print(f"Deleting existing directory: {dest_dir}")
+        shutil.rmtree(dest_dir)
+
+    os.makedirs(dest_dir)
+    print(f"Created directory: {dest_dir}")
+
+    for item in os.listdir(src_dir):
+        src_path = os.path.join(src_dir, item)
+        dest_path = os.path.join(dest_dir, item)
+
+        if os.path.isfile(src_path):
+            shutil.copy(src_path, dest_path)
+            print(f"Copied file: {src_path} -> {dest_path}")
+        elif os.path.isdir(src_path):
+            shutil.copytree(src_path, dest_path)
+            print(f"Copied directory: {src_path} -> {dest_path}")
 
 def main():
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(node)
+    clear_and_copy_static("static", "public")
 
 if __name__ == "__main__":
     main()
-
